2c5e1d9ada4a6ce01680cd1f3d6e7cecce1e2030
WizardlyBump17 <davicraft130@gmail.com>
2025-02-19 16:16:45 -0300

    added the "physics" dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,23 @@ version = "${properties["version"]}-${gitVersion()}"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/WizardlyBump17/physics")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+        }
+    }
 }
 
 val junit = "5.11.0"
+val physics = "0.1.0-75889fc"
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:${junit}"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("com.wizardlybump17.physics:three:${physics}")
 }
 
 tasks {
