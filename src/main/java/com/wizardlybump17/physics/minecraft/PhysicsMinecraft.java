a81b22ea4795ee528f8b88ff56bb5cb4044bbf0f
WizardlyBump17 <davicraft130@gmail.com>
2025-02-20 18:14:09 -0300

    starting the engine

--- src/main/java/com/wizardlybump17/physics/minecraft/PhysicsMinecraft.java
+++ src/main/java/com/wizardlybump17/physics/minecraft/PhysicsMinecraft.java
@@ -3,4 +3,13 @@ package com.wizardlybump17.physics.minecraft;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PhysicsMinecraft extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        startEngine();
+    }
+
+    private void startEngine() {
+        Engine.setObjectContainerRegistry(new BaseObjectContainerRegistry());
+    }
 }
