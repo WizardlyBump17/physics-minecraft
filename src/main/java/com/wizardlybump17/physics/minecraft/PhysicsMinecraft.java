a00e4a096cc418ad89af8dd2e44547bd25962e0f
WizardlyBump17 <davicraft130@gmail.com>
2025-02-20 18:16:08 -0300

    starting the ShapeRendererTask

--- src/main/java/com/wizardlybump17/physics/minecraft/PhysicsMinecraft.java
+++ src/main/java/com/wizardlybump17/physics/minecraft/PhysicsMinecraft.java
@@ -1,15 +1,33 @@
 package com.wizardlybump17.physics.minecraft;
 
+import com.wizardlybump17.physics.minecraft.task.ShapeRendererTask;
+import com.wizardlybump17.physics.three.Engine;
+import com.wizardlybump17.physics.three.registry.BaseObjectContainerRegistry;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PhysicsMinecraft extends JavaPlugin {
 
+    private ShapeRendererTask shapeRendererTask;
+
     @Override
     public void onLoad() {
         startEngine();
+
+        shapeRendererTask = new ShapeRendererTask(Engine.getObjectContainerRegistry());
     }
 
     private void startEngine() {
         Engine.setObjectContainerRegistry(new BaseObjectContainerRegistry());
+    }
+
+    @Override
+    public void onEnable() {
+        shapeRendererTask.runTaskTimer(this, 0, 1);
+    }
+
+    @Override
+    public void onDisable() {
+        if (shapeRendererTask != null)
+            shapeRendererTask.clear();
     }
 }
