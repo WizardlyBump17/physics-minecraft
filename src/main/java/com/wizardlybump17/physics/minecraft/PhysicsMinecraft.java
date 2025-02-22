f318f532e95f40e18e63652e9bd4b4bd0bd225f9
WizardlyBump17 <davicraft130@gmail.com>
2025-02-22 14:04:30 -0300

    added the container and renderers

--- src/main/java/com/wizardlybump17/physics/minecraft/PhysicsMinecraft.java
+++ src/main/java/com/wizardlybump17/physics/minecraft/PhysicsMinecraft.java
@@ -1,11 +1,17 @@
 package com.wizardlybump17.physics.minecraft;
 
 import com.wizardlybump17.physics.minecraft.command.PhysicsCommand;
+import com.wizardlybump17.physics.minecraft.renderer.CubeRenderer;
+import com.wizardlybump17.physics.minecraft.renderer.SphereRenderer;
 import com.wizardlybump17.physics.minecraft.task.ShapeRendererTask;
 import com.wizardlybump17.physics.three.Engine;
+import com.wizardlybump17.physics.three.container.BaseObjectContainer;
+import com.wizardlybump17.physics.three.container.MapBaseObjectContainer;
 import com.wizardlybump17.physics.three.registry.BaseObjectContainerRegistry;
+import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PhysicsMinecraft extends JavaPlugin {
 
@@ -13,18 +19,37 @@ public class PhysicsMinecraft extends JavaPlugin {
 
     @Override
     public void onLoad() {
-        startEngine();
-
         shapeRendererTask = new ShapeRendererTask();
+        startEngine();
+        initRenderers();
     }
 
     private void startEngine() {
-        Engine.setObjectContainerRegistry(new BaseObjectContainerRegistry());
+        BaseObjectContainerRegistry registry = new BaseObjectContainerRegistry();
+        Engine.setObjectContainerRegistry(registry);
+
+        registry.register(new MapBaseObjectContainer(Bukkit.getWorld("world").getUID()));
+    }
+
+    private void initRenderers() {
+        BaseObjectContainerRegistry registry = Engine.getObjectContainerRegistry();
+        for (BaseObjectContainer container : registry.getValues()) {
+            shapeRendererTask.addRenderer(new CubeRenderer(container));
+            shapeRendererTask.addRenderer(new SphereRenderer(container));
+        }
     }
 
     @Override
     public void onEnable() {
         shapeRendererTask.runTaskTimer(this, 0, 1);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (BaseObjectContainer container : Engine.getObjectContainerRegistry().getValues())
+                    container.tick();
+            }
+        }.runTaskTimer(this, 0, 1);
 
         PluginCommand physicsCommand = getCommand("physics");
         PhysicsCommand physicsExecutor = new PhysicsCommand(shapeRendererTask);
