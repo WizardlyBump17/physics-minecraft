bdd87ed42ff37f4e25846306d6dd34a650818b3d
WizardlyBump17 <davicraft130@gmail.com>
2025-03-08 16:29:26 -0300

    added the DebugObjectContainer

---
+++ src/main/java/com/wizardlybump17/physics/minecraft/debug/DebugObjectContainer.java
@@ -0,0 +1,26 @@
+package com.wizardlybump17.physics.minecraft.debug;
+
+import com.wizardlybump17.physics.three.container.MapBaseObjectContainer;
+import org.bukkit.World;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+public class DebugObjectContainer extends MapBaseObjectContainer {
+
+    private final @NotNull World world;
+
+    public DebugObjectContainer(@NotNull World world) {
+        super(getId(world));
+        this.world = world;
+    }
+
+    public @NotNull World getWorld() {
+        return world;
+    }
+
+    public static @NotNull UUID getId(@NotNull World world) {
+        return UUID.nameUUIDFromBytes((world.getName() + "-debug").getBytes(StandardCharsets.UTF_8));
+    }
+}
